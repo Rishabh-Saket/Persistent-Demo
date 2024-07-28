02327ffb554c5b4549c45b21758e90f80fc7f5e3
Rishabh-Saket <87156299+Rishabh-Saket@users.noreply.github.com>
2024-07-28 23:40:19 +0530

    Added a employee entity for JPA demo

---
+++ src/main/java/com/workshop/persistent/models/EmployeeRole.java
@@ -0,0 +1,6 @@
+package com.workshop.persistent.models;
+
+public enum EmployeeRole 
+{
+	ROLE_USER, ROLE_ADMIN
+}
---
+++ src/test/java/com/workshop/persistent/PersistentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.persistent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
